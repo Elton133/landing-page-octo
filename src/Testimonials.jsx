059d43c2eb97d677886402500727c0b6f0e90b1c
Elton133 <134627073+Elton133@users.noreply.github.com>
2024-12-08 14:47:29 +0000

    App Landing Page

--- src/Testimonials.jsx
+++ src/Testimonials.jsx
@@ -1,4 +1,4 @@
-
+import vr from './assets/vr.jpg'
 
 
 export default function Testimonials() {
@@ -10,9 +10,9 @@ export default function Testimonials() {
         <p className="testimonials-description">Learn how this powerful tool has revolutionized thier work and accelerated their success.</p>
         </div>
         <div className="testimonials">
-        <p>"The excellent voice recognition quality of AI Ally app makes it perfect for dictation and note taking. I have never come across such accuracy before" <br /><br /><br /><img src="" alt="" />Craig Jordan <br />CEO, Aveva</p>
-        <p>"The excellent voice recognition quality of AI Ally app makes it perfect for dictation and note taking. I have never come across such accuracy before" <br /><br /><br /><img src="" alt="" />Craig Jordan <br />CEO, Aveva</p>
-        <p>"The excellent voice recognition quality of AI Ally app makes it perfect for dictation and note taking. I have never come across such accuracy before" <br /><br /><br /><img src="" alt="" />Craig Jordan <br />CEO, Aveva</p>
+        <p>"The excellent voice recognition quality of AI Ally app makes it perfect for dictation and note taking. I have never come across such accuracy before" <br /><br /><br /><img className="person-image" src={vr} alt="person-image" />Craig Jordan <br />CEO, Aveva</p>
+        <p>"The excellent voice recognition quality of AI Ally app makes it perfect for dictation and note taking. I have never come across such accuracy before" <br /><br /><br /><img className="person-image" src={vr} alt="person-image" />Craig Jordan <br />CEO, Aveva</p>
+        <p>"The excellent voice recognition quality of AI Ally app makes it perfect for dictation and note taking. I have never come across such accuracy before" <br /><br /><br /><img className='person-image' src={vr} alt="person-image" />Craig Jordan <br />CEO, Aveva</p>
         </div>
         </>
     )
