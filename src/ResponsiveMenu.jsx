735c67c0a0651b57ef3001b3fb0fc91938df55bf
Elton133 <134627073+Elton133@users.noreply.github.com>
2024-12-08 23:40:13 +0000

    App Landing Page

---
+++ src/ResponsiveMenu.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+
+export default function ResponsiveMenu({ open }) {
+    return (
+        <AnimatePresence>
+            {open && (
+                <motion.div
+                    initial={{ opacity: 0 , y: -100}}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y:-100 }}
+                    transition={{ duration: 0.3 }}
+                    className="absolute top-20 left-0 w-full h-screen z-20"
+                >
+                    <div className='text-xl font-semibold bg-black text-white py-10 m-6 rounded-3xl'>
+                        <ul className="flex flex-col justify-center items-center gap-10">
+                            <li>Home</li>
+                            <li>Features</li>
+                            <li>Pricing</li>
+                            <li>Testimonials</li>
+                        </ul>
+                    </div>
+                </motion.div>
+            )}
+        </AnimatePresence>
+    );
+}
