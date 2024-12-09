5dcc9c8673af8da8385dc6f3b0a55a5c53874167
Elton133 <134627073+Elton133@users.noreply.github.com>
2024-12-09 20:32:58 +0000

    App Landing Page

--- src/ResponsiveMenu.jsx
+++ src/ResponsiveMenu.jsx
@@ -10,7 +10,7 @@ export default function ResponsiveMenu({ open }) {
                     animate={{ opacity: 1, y: 0 }}
                     exit={{ opacity: 0, y:-100 }}
                     transition={{ duration: 0.3 }}
-                    className="absolute top-20 left-0 w-full h-screen z-20 bg-black/60 backdrop-blur-sm"
+                    className="absolute top-20 left-0 w-full h-screen z-20 bg-transparent/60 backdrop-blur-sm"
                 >
                     <div className='text-xl font-semibold  backdrop-blur-lg text-white py-20 m-6 rounded-3xl border-gray-500 border-2'>
                         <ul className="flex flex-col justify-center items-center gap-5">
